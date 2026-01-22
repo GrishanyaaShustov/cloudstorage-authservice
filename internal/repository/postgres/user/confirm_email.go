3a571a0d18a77d70573da6a910aed0d1d7d3bfa9
Grigoriy <dinsuil275283@gmail.com>
2026-01-23 04:23:14 +0500

    Create user repository method: ConfirmEmail

---
+++ internal/repository/postgres/user/confirm_email.go
@@ -0,0 +1,29 @@
+package userrepo
+
+import (
+	"context"
+	"fmt"
+)
+
+const confirmEmailQuery = `
+	UPDATE users
+	SET email_verified_at = now()
+	WHERE id = $1
+	  AND email_verified_at IS NULL;
+`
+
+func (r *UserRepository) ConfirmEmail(ctx context.Context, userID string) error {
+	cmd, err := r.pool.Exec(ctx, confirmEmailQuery, userID)
+	if err != nil {
+		if isUnavailable(err) {
+			return ErrUnavailable
+		}
+		return fmt.Errorf("%w: confirm email", ErrInternal)
+	}
+
+	if cmd.RowsAffected() == 0 {
+		return ErrNotFound
+	}
+
+	return nil
+}
